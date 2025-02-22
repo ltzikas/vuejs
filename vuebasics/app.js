bf211fb3831a40e85d0612767a5e32af9c98ca96
ltzikas <39968493+ltzikas@users.noreply.github.com>
2025-02-22 02:29:52 -0300

    Lesson 15

--- vuebasics/app.js
+++ vuebasics/app.js
@@ -1,14 +1,36 @@
 const app = Vue.createApp({
     data(){
         return {
+            url: "http://www.thenetninja.co.uk",
+            showBooks: true,
             title: "El imperio contraataca",
             author: "Mark Hamill",
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {title: "El señor de los anillos", author: "J.R.R. Tolkien", img: "assets/1.jpg", isFav: true},
+                {title: "Harry Potter", author: "J.K. Rowling", img: "assets/2.jpg", isFav: false},
+                {title: "Cien años de soledad", author: "Gabriel García Márquez", img: "assets/3.jpg", isFav: true}
+            ]
         }
     },
     methods: {
         changeTitle(title){
                 this.title = title
+        },
+        toogleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
